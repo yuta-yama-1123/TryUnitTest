367e59b3df273409752f15682c9f7f527f73f878
yuta-yama-1123 <yutaforios@gmail.com>
2022-09-11 15:52:51 +0900

    Initial Commit

---
+++ TryUnitTest/TryUnitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryUnitTestApp.swift
+//  TryUnitTest
+//
+//  Created by 山本祐太 on 2022/09/11.
+//
+
+import SwiftUI
+
+@main
+struct TryUnitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TryUnitTestUITests/TryUnitTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TryUnitTestUITestsLaunchTests.swift
+//  TryUnitTestUITests
+//
+//  Created by 山本祐太 on 2022/09/11.
+//
+
+import XCTest
+
+class TryUnitTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
